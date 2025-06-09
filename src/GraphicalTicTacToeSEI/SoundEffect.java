ebd23202ed8b0dfea627ea56f5444508e725e1a4
Nayla <naylarameyzaa@gmail.com>
2025-06-09 22:44:58 +0700

    GraphicalSEI (image and sound effect)

--- src/GraphicalTicTacToeSEI/SoundEffect.java
+++ src/GraphicalTicTacToeSEI/SoundEffect.java
@@ -20,9 +20,10 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  * For Eclipse, place the audio file under "src", which will be copied into "bin".
  */
 public enum SoundEffect {
-    EAT_FOOD("audio/eatfood.wav"),
-    EXPLODE("audio/explode.wav"),
-    DIE("audio/die.wav");
+    CLICK("audio/click.wav"),
+    WIN("audio/WIN.wav"),
+    SERI("audio/seri.wav");
+
 
     /** Nested enumeration for specifying volume */
     public static enum Volume {
