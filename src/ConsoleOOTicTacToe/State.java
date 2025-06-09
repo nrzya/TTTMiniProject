8c8ed99d85ae201b790a36f02deb62944bfac900
Nayla <naylarameyzaa@gmail.com>
2025-06-09 21:31:49 +0700

    GraphicalTTTSimpleOO

--- src/ConsoleOOTicTacToe/State.java
+++ src/ConsoleOOTicTacToe/State.java
@@ -1,10 +1,4 @@
 package ConsoleOOTicTacToe;
-// Define named constants to represent the various states of the game
-public static final int PLAYING    = 0;
-public static final int DRAW       = 1;
-public static final int CROSS_WON  = 2;
-public static final int NOUGHT_WON = 3;
-
-// The current state of the game
-public static int currentState = PLAYING;  // Assigned to a named constant, which is easier to read
-// and understand, instead of an int number 0
+public enum State {
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
